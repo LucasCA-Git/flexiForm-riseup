7554e8a17a06d156786c26d6490b0015a9268b30
LucasCA-Git <lucascardosoalecrim99@gmail.com>
2025-09-26 17:34:45 -0300

    Exporta o componente para que projetos externos possam usá-lo (LIB)

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+// src/index.jsx
+// Este é o ponto de entrada principal para a sua biblioteca (lib).
+import DynamicForm from './components/DynamicForm';
+
+// Exporta o componente para que projetos externos possam usá-lo.
+export { DynamicForm };
